a84bd281931dd2806efe841f550beb92c97ab051
but0n <but0n@icloud.com>
2017-02-05 21:38:28 +0800

    feat: (ruler)

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -12,13 +12,16 @@ $(() => {
 	// 	alert("Ok");
 	// }
 	window.addEventListener('deviceorientation', (e)=>{
-		let pitch = e.gamma;
+		let pitch = -e.gamma;
 		let roll = -e.beta;
 		let yaw = -e.alpha;
-		if (pitch > 0)
-			roll = 180 - roll;
-		$(".label").eq(0).text(Math.round(pitch));
-		$(".label").eq(1).text(Math.round(roll));
+		g__pitch = pitch;
+		g__roll = roll;
+		g__yaw = yaw;
+		if (pitch < 0)
+			roll = -180 - roll;
+		// $(".label").eq(0).text(Math.round(pitch));
+		// $(".label").eq(1).text(Math.round(roll));
 		HUD.fresh(pitch, roll, yaw);
 	}, false);
 	// setInterval(UnitTest, 25);
@@ -71,7 +74,7 @@ function dashboard(component) {
 		let cxt = this.cxt;
 		let space = this.aimLenght;
 		cxt.save()
-		cxt.translate(0, p);
+		cxt.translate(0, this.height/2 * ((p>0?p-this.pitchOffset:p+this.pitchOffset)/90));
 		cxt.rotate(r*Math.PI/180);
 
 		cxt.beginPath();
@@ -83,7 +86,7 @@ function dashboard(component) {
 		cxt.stroke();
 
 		cxt.restore();
-	}
+	};
 
 	this.drawAim = () => {
 		let cxt = this.cxt;
@@ -122,23 +125,85 @@ function dashboard(component) {
 		cxt.lineTo(-this.aimLenght, -delta);
 
 		cxt.stroke();
-	}
 
-	this.drawAimLine = (cxt, spot, delta) => {
-		cxt.moveTo(spot, 0);
-		cxt.lineTo(25, 0);
+		this.drawRuler(mainSpot, delta);
+	};
 
-		cxt.moveTo(spot, 0);
-		cxt.lineTo(spot+delta, delta);
-		cxt.lineTo(this.aimLenght, delta);
+	this.drawRuler = (x, y) => {
+		let cxt = this.cxt;
+		let angle = g__pitch>0?g__pitch-this.pitchOffset:g__pitch+this.pitchOffset
+		cxt.font = y*2+"px Menlo";
+		cxt.fillStyle = this.theme.color;
+		cxt.fillText(-Math.round(angle), x+y, y-2);
 
-		cxt.moveTo(spot, 0);
-		cxt.lineTo(spot+delta, -delta);
-		cxt.lineTo(this.aimLenght, -delta);
+		// Ruler
+		let x_offset = this.aimLenght/2*.7;
+		let unitLength = 8;
+		let space = unitLength;
+		let item_width = 20;
+		let item_height = 1;
+		let fontSize = 10;
+		let label_cap = 5;
 
-	}
+		cxt.save();
+		cxt.translate(x_offset, angle*unitLength);
+		for(let d = 0; d <180; d++) {
+			if(!(d%10)) {
+				// line
+				cxt.fillStyle = "rgba(255, 255, 255, 1)";
+				cxt.fillRect(-item_width/2, 0, item_width, item_height);
+				// label
+				cxt.fillStyle = this.theme.color;
+				cxt.font = fontSize+"px Menlo"
+				cxt.fillText(-d, -item_width*2, fontSize/2);
+				cxt.translate(0, space);
+			} else {
+				if(!(d%5)) {
+					cxt.fillStyle = "rgba(255, 255, 255, 1)";
+					cxt.fillRect(-item_width/2/2, 0, item_width/2, item_height);
+					cxt.translate(0, space);
 
-}
+				} else {
+					cxt.fillStyle = "rgba(255, 255, 255, .3)"
+					cxt.beginPath();
+					cxt.arc(0, 0, item_height, 0, 2*Math.PI);
+					cxt.fill();
+					cxt.translate(0, space);
+				}
+			}
+		}
+		cxt.restore();
+		// Up
+		cxt.save();
+		cxt.translate(x_offset, angle*unitLength);
+		for(let d = 0; d >-180; d--) {
+			if(!(d%10)) {
+				// line
+				cxt.fillStyle = "rgba(255, 255, 255, 1)";
+				cxt.fillRect(-item_width/2, 0, item_width, item_height);
+				// label
+				cxt.fillStyle = this.theme.color;
+				cxt.font = fontSize+"px Menlo"
+				cxt.fillText(-d, -item_width*2, fontSize/2);
+				cxt.translate(0, -space);
+			} else {
+				if(!(d%5)) {
+					cxt.fillStyle = "rgba(255, 255, 255, 1)";
+					cxt.fillRect(-item_width/2/2, 0, item_width/2, item_height);
+					cxt.translate(0, -space);
+
+				} else {
+					cxt.fillStyle = "rgba(255, 255, 255, .3)"
+					cxt.beginPath();
+					cxt.arc(0, 0, item_height, 0, 2*Math.PI);
+					cxt.fill();
+					cxt.translate(0, -space);
+				}
+			}
+		}
+		cxt.restore();
+	};
+};
 // function loop() {
 //     cxt.fillStyle = "#333333";
 //     cxt.fillRect(0, 0, 600, 600);
